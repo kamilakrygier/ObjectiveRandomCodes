26277cbe5bdd3281d2dc5d7cd80f3efca523d190
kamilakrygier <kamilakrygier2308@gmail.com>
2022-11-22 22:24:42 +0100

    Added a new program which calculate a average of given grades

---
+++ include/GradeAverage.hpp
@@ -0,0 +1,13 @@
+class GradeAverage
+{
+private:
+    int choose;
+    float average;
+public:
+    GradeAverage ();
+    void setChoose (int chose1);
+    void numberOfGrades();
+    float averageOfGrades();
+    void printAverage();
+    void redStripe();
+};
