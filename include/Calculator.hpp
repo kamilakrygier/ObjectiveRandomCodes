ec141e0fd46510179b20038c354738f173dbb9ea
kamilakrygier <kamilakrygier2308@gmail.com>
2022-11-26 17:08:32 +0100

    Added a caluclator

---
+++ include/Calculator.hpp
@@ -0,0 +1,17 @@
+#ifndef CALCULATOR_HPP
+#define CALCULATOR_HPP
+
+class Calculator
+{
+private:
+  float number;
+  float result;
+  char operation;
+public:
+  Calculator();
+  void actions();
+  float operationsOfTheCalculator ();
+  void result1();
+};
+
+#endif
